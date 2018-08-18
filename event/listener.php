d89419763f2354fecc9255923c2f5539e3eca245
v12mike <morrinmike@gmail.com>
2018-08-18 05:58:45 +0100

    Optimise listener

--- event/listener.php
+++ event/listener.php
@@ -7,24 +7,18 @@
 * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
 *
 */
-
 namespace v12mike\imageredirect\event;
-
 define('ONE_MONTH', '2500000'); //seconds (approximately)
 define('REGEX_STRING', '#<img [^>]*src="(http(s?):\/\/[^"]+)"[^>]*>#'); //define it once here, in case it needs adjusting
-
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-
 class listener implements EventSubscriberInterface
 {
-
 	static public function getSubscribedEvents()
 	{
 		return array(
 			'core.page_footer_after'	=> 'rewrite_assets',
 		);
 	}
-
 	/**
 	 * Rewrites an image tag into a version that can be used by a Camo asset server
 	 *
@@ -35,7 +29,6 @@ class listener implements EventSubscriberInterface
 	private function rewrite_images(&$object, $key, $domains, $local_images_mode, $local_images_path)
 	{
 		global $phpbb_root_path;
-
 		if (!empty($object[$key]))
 		{
 			if (preg_match_all(REGEX_STRING, $object[$key], $matches))
@@ -52,11 +45,15 @@ class listener implements EventSubscriberInterface
 						{
 							// we will link to the local file
 							$object[$key] = str_replace($url, generate_board_url() . '/' . $local_images_path . $local_file_name, $object[$key]);
-
 							// next please!
 							continue;
 						}
 					}
+					
+					// skip unless proxy mode enabled
+					if ($this->config['imageredirect_proxymode'] == 0) 
+						continue;
+					
 					foreach ($domains as $row)
 					{
 						$domain = $row['domain'] . '/' ;
@@ -92,7 +89,6 @@ class listener implements EventSubscriberInterface
 			}
 		}
 	}
-
 	/**
 	 * Adds an unhandled insecure link location to the database
 	 *
@@ -105,7 +101,6 @@ class listener implements EventSubscriberInterface
 	private function unhandled_insecure_link($location, $field, &$locations)
 	{
 		global $cache;
-
 		foreach ($locations as $configured_location)
 		{
 			if (($configured_location['location'] == $location) && ($configured_location['field'] == $field))
@@ -141,22 +136,16 @@ class listener implements EventSubscriberInterface
 		$result = $this->db->sql_query($sql);
 		$locations = $this->db->sql_fetchrowset($result);
 		$this->db->sql_freeresult($result);
-
-
 	}
-
 	public function rewrite_assets($event)
 	{
 		global $request;
 		global $phpbb_container;
-
 		if (($this->config['imageredirect_proxymode'] == 0) && ($this->config['imageredirect_localimagesmode'] == 0))
 			return;
-
 		$context = $phpbb_container->get('template_context');
 		$rootref = &$context->get_root_ref();
 		$tpldata = &$context->get_data_ref();
-
 		// get all the domains that are directly remapped
 		// do it here for efficiency
 		$sql = 'SELECT domain, subdomains FROM ' . $this->table_prefix . 'imageredirect_no_proxy_domains';
@@ -166,14 +155,12 @@ class listener implements EventSubscriberInterface
 		$this->db->sql_freeresult($result);
 		$local_images_mode = $this->config['imageredirect_localimagesmode'];
 		$local_images_path = $this->config['imageredirect_localimagespath'];
-
 		// get all the fields that need to be patched
 		$sql = 'SELECT location, field, type, location_id FROM ' . $this->table_prefix . 'imageredirect_link_locations';
 		// cache the query for a while 
 		$result = $this->db->sql_query($sql, ONE_MONTH);
 		$locations = $this->db->sql_fetchrowset($result);
 		$this->db->sql_freeresult($result);
-
 		foreach ($locations as $row)
 		{
 			if ($row['type'] == 0)
@@ -199,7 +186,6 @@ class listener implements EventSubscriberInterface
 				}
 			}
 		}
-
 		// catch any http:// image links and add to database
 		// only if in 'learning mode' and user is admin
 		if (($this->config['imageredirect_proxymode'] == 2) && $this->auth->acl_get('a_'))
@@ -222,9 +208,7 @@ class listener implements EventSubscriberInterface
 				}
 			}
 		}
-
 	}
-
 	public function __construct(\phpbb\config\config $config, \phpbb\db\driver\driver_interface $db, $table_prefix, $auth, $user)
 	{
 	   $this->config = $config;
